308930bce5f25e31fb5df9b8bbb659370639acb6
Sterling West <sterling-west@utulsa.edu>
2012-03-27 17:20:20 -0500

    Changed eqn-solve to auto-restart

--- eqn-solve.py
+++ eqn-solve.py
@@ -1,18 +1,13 @@
-eqn = raw_input()
-while(len(eqn) > 0 and eqn[-1]=='\n'): eqn = eqn[:-1]
-eqn = eqn.split('=')
-eqn = eqn[0] + '-' + '(' + eqn[1] + ')'
-
-def solve(f):
-	tol = 0.0000001
-	p0 = 0.
-	p1 = 1.
-	i = 0
-	while(abs(f(p0)) > tol and abs(p1-p0) > tol and i < 10000):
-		p0,p1  = p0 - f(p0)*(p0-p1)/(f(p0)-f(p1)), p0
-		i += 1
-	if(i == 10000): return None
+import random
+def secant_method(f):
+	i, p0, p1, tol = 0.,80.,79.,0.000000
+	while(abs(p1-p0) > tol and i < 10000):
+		if(i%1000 == 0):
+			p0 = random.random()*200000 -100000
+			p1 = p0+1
+		p0,p1,i  = p0 - f(p0)*(p0-p1)/(f(p0)-f(p1)), p0,i+1
+	if(i == 100000): return 'Failure'
 	return round(p0,5)
-
+eqn = '-'.join(['('+e+')' for e in raw_input().split('=')])
 f = lambda x: eval(eqn)
-print solve(f)
+print secant_method(f)
